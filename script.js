11d644889925e1f77c86a7a4fd0d5a5b0c7efd54
joaogabrielfautinowilamoskidasilva <joao.wilamoski.silva@escola.pr.gov.br>
2025-03-26 14:27:18 +0000

    Terminamos a aula 4.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao document.getElementById('aumentar-fonte');
+    const aumentaFonteBotao = document.getElementById ('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
