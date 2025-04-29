2aaf85361a33b6502cbbe1d4e0ba48243d7f31ce
joaogabrielfautinowilamoskidasilva <joao.wilamoski.silva@escola.pr.gov.br>
2025-04-29 12:15:37 +0000

    Até aula 6

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById ('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    const alternaContraste = document.getElementById('alterna-contraste');
+    
     let tamanhoAtualFonte = 1;
 
     aumentaFonteBotao.addEventListener('click', function(){
@@ -15,6 +17,10 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
+    });
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
     });
 
     botaoDeAcessibilidade.addEventListener('click', function (){
